57d42d16212783fa4af2d3b947e6668eea67b3a8
Timothy Kipkorir Serem <theecodepoet@gmail.com>
2021-10-17 19:01:45 +0300

    Modified countries db to fetch data from room or json

--- app/src/main/java/com/kiptechie/androidpagingsample/repository/CountriesDb.kt
+++ app/src/main/java/com/kiptechie/androidpagingsample/repository/CountriesDb.kt
@@ -3,14 +3,20 @@ package com.kiptechie.androidpagingsample.repository
 import android.content.Context
 import com.google.gson.Gson
 import com.kiptechie.androidpagingsample.data.models.Country
+import com.kiptechie.androidpagingsample.data.room.CountryDao
+import com.kiptechie.androidpagingsample.data.room.RoomModule
+import java.util.concurrent.Executors
 
 class CountriesDb {
 
     companion object {
 
         private lateinit var countries: ArrayList<Country>
+        private var isRoom = false
+        private lateinit var dao: CountryDao
 
-        fun initialize(context: Context) {
+        fun initialize(context: Context, isRoom: Boolean) {
+            this.isRoom = isRoom
             val content = context.assets.open("countries_paged.json")
                 .bufferedReader()
                 .use {
@@ -20,18 +26,45 @@ class CountriesDb {
                 Gson().fromJson(content, Array<Country>::class.java)
                     .toList()
             )
+            dao = RoomModule.getInstance(context).countryDao()
+            if (isRoom) {
+                Executors.newCachedThreadPool().submit {
+                    for (country in countries) {
+                        dao.insert(country)
+                    }
+                }
+            }
         }
 
         fun getCountries(): List<Country> {
-            return countries
+            return if (isRoom) {
+                dao.getAllCountries()
+            } else {
+                countries
+            }
         }
 
         fun deleteCountryById(id: Int) {
-            countries.removeAll { country -> country.id == id }
+            if (isRoom) {
+                Executors.newSingleThreadExecutor().submit {
+                    val country = dao.findCountryById(id)
+                    if (country != null) {
+                        dao.delete(country)
+                    }
+                }
+            } else {
+                countries.removeAll { country -> country.id == id }
+            }
         }
 
         fun addCountry(country: Country) {
-            countries.add(country)
+            if (isRoom) {
+                Executors.newSingleThreadExecutor().submit {
+                    dao.insert(country)
+                }
+            } else {
+                countries.add(country)
+            }
         }
 
     }
