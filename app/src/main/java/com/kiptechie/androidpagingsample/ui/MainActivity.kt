9fcd4e0fb9eeede9948dde64bbd2f3992aa985fb
Timothy Kipkorir Serem <theecodepoet@gmail.com>
2021-10-17 19:03:12 +0300

    Add listener for user choice on room or json data

--- app/src/main/java/com/kiptechie/androidpagingsample/ui/MainActivity.kt
+++ app/src/main/java/com/kiptechie/androidpagingsample/ui/MainActivity.kt
@@ -4,33 +4,32 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.AsyncDifferConfig
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.kiptechie.androidpagingsample.databinding.ActivityMainBinding
-import com.kiptechie.androidpagingsample.repository.CountriesDb
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
     private val mainViewModel: MainViewModel by viewModels()
     private val TAG = MainActivity::class.java.simpleName
+    var isRoom = false
+    private lateinit var onDataSourceChosen: OnDataSourceChosen
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
-        CountriesDb.initialize(this)
-
-        val countryPagedListAdapterDiffUtil = CountryPagedListAdapterDiffUtil()
-        val asyncPagedListAdapterDiffUtil =
-            AsyncDifferConfig.Builder(countryPagedListAdapterDiffUtil).build()
-        val countriesPagedAdapter = CountryPagedListAdapter(asyncPagedListAdapterDiffUtil)
-        setUpRecycler(countriesPagedAdapter)
+        onDataSourceChosen = OnDataSourceChosen(this)
+        binding.jsonBtn.setOnClickListener {
+            onDataSourceChosen.onDataChosen(false)
+        }
+        binding.roomBtn.setOnClickListener {
+            onDataSourceChosen.onDataChosen(true)
+        }
     }
 
-    private fun setUpRecycler(countriesPagedAdapter: CountryPagedListAdapter) {
+    fun setUpRecycler(countriesPagedAdapter: CountryPagedListAdapter) {
         binding.pagingRv.layoutManager = LinearLayoutManager(this)
         binding.pagingRv.adapter = countriesPagedAdapter
         setUpObservers(countriesPagedAdapter)
